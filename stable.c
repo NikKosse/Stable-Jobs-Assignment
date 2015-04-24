b07f3fd11fd5887ff665221754b8356df950d697
Nikolas Kosse <knk2r7@babbage.cs.missouri.edu>
2015-04-24 18:43:06 -0500

    pushing mnost recent versions

--- stable.c
+++ stable.c
@@ -2,31 +2,54 @@
 #include <string.h>
 #include <stdlib.h>
 
-#define size 5
-#define sName 50
+#define numJob 2
+#define numApp 4
 
 typedef struct {
-	char name[sName];
-	char prefrences[sName][sName];
+	char first[100];
+	char last[100];
+	char pref[10][10];
 	int hasJob;
-	char job_title[sName];
 } Applicant;
 
 typedef struct {
-	char name[sName];
-	char prefrences[sName][sName];
+	char jobName[100];
+	char pref[10][10];
 	int openings;
 	int full;
-	char employees[sName][sName];
+	char employees[10][10];
 } Job;
 
 
 int main() {
-	printf("fycker");
 	system("clear");
 	Applicant *applicants;
-	applicants = malloc(size * sizeof(Applicant));
+	applicants = malloc(numApp * sizeof(Applicant));
 	
 	Job *jobs;
-	jobs = malloc(size * sizeof(Job));
+	jobs = malloc(numJob * sizeof(Job));
+	
+	applicants[0].first = "John";
+	applicants[0].last = "Doe";
+	applicants[0].pref = "By", "Ax";
+	applicants[0].hasJob = 0;
+	applicants[0].job_title = NULL;
+
+	applicants[1].first = "Jane";
+	applicants[1].last = "Doe";
+        applicants[1].pref = "Ax","By";
+        applicants[1].hasJob = 0;
+        applicants[1].job_title = NULL;
+
+        applicants[2].first = "Max";
+	applicants[2].last = "Doe";
+        applicants[2].pref = "Ax","By";
+        applicants[2].hasJob = 0;
+        applicants[2].job_title = NULL;
+
+        applicants[3].first = "Min";
+	applicants[3].last = "Doe";
+        applicants[3].pref = "By","Ax";
+        applicants[3].hasJob = 0;
+        applicants[3].job_title = NULL;
 }

5e2c08bba3c41eec6bee9ef3e39c76926c11d781
Nikolas Kosse <knk2r7@babbage.cs.missouri.edu>
2015-05-01 15:36:27 -0500

    now I added the header file

---
+++ struct.h
@@ -0,0 +1,17 @@
+struct Job {
+    char jobName[100];
+    char pref[10][10];
+    int openings;
+    int full;
+    int employees[10][10];
+};
+
+struct People {
+    char first[100];
+    char last[100];
+    char pref[10][10];
+    int hasJob;
+    char job_title[100];
+};
+
+

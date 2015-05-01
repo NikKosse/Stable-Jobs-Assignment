7e5dc91bac3bc6a0300af3b838871eb9c5211728
mabrm9 <mabrm9@mail.missouri.edu>
2015-05-01 15:48:40 -0500

    makefile added

---
+++ jobSearch.h
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+struct Job {
+    char jobName[100];
+    char pref[10][10];
+    int openings;
+    int full;
+    int employees[10][10];
+};
+
+struct People {
+    char first[100];
+    char last[100];
+    char pref[10][10];
+    int hasJob;
+    char job_title[100];
+};
+
+
